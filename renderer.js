09dee48ae5ee935a20d9a9bc993f46407a2d4236
AhmedBaba95 <ahmedbabamohamed95@gmail.com>
2023-10-25 14:24:52 +0000

    elementToString function

--- renderer.js
+++ renderer.js
@@ -7,10 +7,6 @@ function createElement(type, props, ...children) {
                 if (typeof child === 'string' || typeof child === 'number' || typeof child === 'object') {
                     return child;
                 }
-                // if (Array.isArray(child)) {
-                //      return child.map(c => c
-                //     );
-                // }
             }),
         },
         toString: function() {
@@ -19,11 +15,16 @@ function createElement(type, props, ...children) {
     };
 }
 
+function elementToString(createdElement) {
+    console.log(createdElement.toString());
+}
+
 let a = createElement("div");
 let b = createElement("div", { id: "home" }, "a", a);
 let c = createElement("div", null, 1, 6, [b]);
- 
-// console.log(a.toString());
-// console.log(b.toString());
-console.log(c.toString());
+
+a1 = elementToString(a);
+b1 = elementToString(b);
+c1 = elementToString(c);
+
 
