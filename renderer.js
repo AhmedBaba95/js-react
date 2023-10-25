cea64a59e07eaf0783e2b02bab49ee57e1302791
AhmedBaba95 <ahmedbabamohamed95@gmail.com>
2023-10-25 14:02:02 +0000

    typeof child === 'object'

--- renderer.js
+++ renderer.js
@@ -4,27 +4,26 @@ function createElement(type, props, ...children) {
          props: {
               ...props,
              children: children.flatMap(child => {
-                if (typeof child === 'string' || typeof child === 'number') {
+                if (typeof child === 'string' || typeof child === 'number' || typeof child === 'object') {
                     return child;
                 }
-                if (Array.isArray(child)) {
-                     return child.map(c =>
-                         typeof c === 'object' ? c : c
-                    );
-                }
+                // if (Array.isArray(child)) {
+                //      return child.map(c => c
+                //     );
+                // }
             }),
         },
-        // toString: function() {
-        //     return `{"type": '${this.type}',\n "props": ${JSON.stringify(this.props, null, 2)}}`;
-        // },
+        toString: function() {
+            return `{"type": '${this.type}',\n "props": ${JSON.stringify(this.props, null, 2)}}`;
+        },
     };
 }
 
 let a = createElement("div");
-let b = createElement("div", { id: "home" }, "a");
-let c = createElement("div", null, 1, "B");
+let b = createElement("div", { id: "home" }, "a", a);
+let c = createElement("div", null, 1, 6, [b]);
  
 // console.log(a.toString());
 // console.log(b.toString());
-// console.log(c.toString());
+console.log(c.toString());
 
